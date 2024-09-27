75f28a7c79c6454aace4b907de3378e10c864140
unknown <241880161@smail.nju.edu.cn>
2024-09-27 08:42:27 +0800

    1-type-oi

---
+++ 1-types-io/circle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int radius = 10;
+    double circumference = 2 * 3.14159 * radius;
+    double area = 3.14159 *radius * radius;
+
+    printf("radius = %d\n circumference = %.2f\n area=%.2f\n", radius,circumference,area );
+    return 0;
+
+}
